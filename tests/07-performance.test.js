91e055b3817bf7f04ce206075b5007c573f51647
Ramakrishnan Kandasamy <rmkanda@thoughtworks.com>
2020-09-30 12:41:53 +0530

    [Ramki] Add lighthouse tests

--- tests/07-performance.test.js
+++ tests/07-performance.test.js
@@ -1,9 +1,12 @@
+const fs = require("fs");
 const puppeteer = require("puppeteer");
 const { expect } = require("chai");
+const lighthouse = require("lighthouse");
+const reportGenerator = require("lighthouse/lighthouse-core/report/report-generator");
 
 const APP_URL = "https://rmkanda.github.io/sample-pwa/";
 
-describe("Performance metrics", async () => {
+describe("Performance tests", async () => {
   it("site should take only 5MB of heap size", async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
@@ -13,6 +16,35 @@ describe("Performance metrics", async () => {
     const metrics = await page.metrics();
 
     expect(metrics.JSHeapTotalSize).lessThan(5 * 1024 * 1024);
+
+    await browser.close();
+  });
+
+  it("seo score should be 0.91", async () => {
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: [`--remote-debugging-port=${8042}`],
+    });
+    const lighthouseResult = await lighthouse(APP_URL, {
+      port: 8042,
+      disableStorageReset: true,
+    });
+
+    expect(lighthouseResult.lhr.categories["seo"].score).equals(0.91);
+
+    const time_to_interactive =
+      lighthouseResult.lhr.audits["interactive"].displayValue;
+
+    console.log(`Time To Interactive: ${time_to_interactive}`);
+    console.log("Lighthouse metrics", lighthouseResult.lhr.audits);
+
+    //Report
+    const html = reportGenerator.generateReport(lighthouseResult.lhr, "html");
+    fs.writeFile("LightHouseReport.html", html, (err) => {
+      if (err) {
+        console.error(err);
+      }
+    });
 
     await browser.close();
   });
